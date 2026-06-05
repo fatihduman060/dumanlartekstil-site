cae83a3947604e324d251ad43f3462ab4ac4f533
fatihduman060 <fatihduman060@gmail.com>
2026-06-05 10:47:33 +0300

    Delete muhasebe directory

--- muhasebe/assets/muhasebe.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('click', function (event) {
-  const toggle = event.target.closest('[data-menu-toggle]');
-  if (toggle) document.body.classList.toggle('sidebar-open');
-});
