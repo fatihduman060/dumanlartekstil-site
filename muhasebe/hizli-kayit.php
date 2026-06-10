62cf88109acc2d022cee9f16631587001cfde337
Ahmet1991 <ahmet.aktss@gmail.com>
2026-06-11 00:05:19 +0300

    Delete muhasebe directory

--- muhasebe/hizli-kayit.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-flash('info', 'Hızlı kayıt ekranı sadeleştirme için kaldırıldı. İşlemleri Hareketler, Çekler veya Belgeler ekranından yapabilirsiniz.');
-redirect('hareketler.php');
--- muhasebe/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-if (is_logged_in()) {
-    log_action('Çıkış', 'Oturum kapatıldı');
-}
-destroy_session_cookie();
-redirect('index.php?logged_out=1');
