3c83ff231219aa49998cbe05df3d6a8750dc1daf
Ahmet1991 <ahmet.aktss@gmail.com>
2026-06-11 01:01:26 +0300

    Çek renk CSS dosyasını yükle

--- muhasebe/layout.php
+++ muhasebe/layout.php
@@ -35,6 +35,7 @@ function page_header(string $title, string $active = ''): void
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="assets/muhasebe.css?v=515" />
+  <link rel="stylesheet" href="assets/cek-renkleri.css?v=1" />
 </head>
 <body class="app-page">
   <div class="app-shell">
