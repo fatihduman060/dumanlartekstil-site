bdf8187d8e7acf69a92e819c2346e61107553f83
fatihduman060 <fatihduman060@gmail.com>
2026-06-30 10:41:29 +0300

    Teklif hesap düzeltme scriptini panele bağla

--- muhasebe/layout.php
+++ muhasebe/layout.php
@@ -172,6 +172,7 @@ function page_footer(): void
   <script src="assets/muhasebe.js?v=516"></script>
   <script src="assets/super-admin-role.js?v=1"></script>
   <script src="assets/muhasebe-polish.js?v=1"></script>
+  <script src="assets/teklif-hesap-fix.js?v=1"></script>
   <script src="assets/cek-vade-uyari.js?v=2"></script>
 </body>
 </html>
