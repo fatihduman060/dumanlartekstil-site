95e7902225cf68d6a5253d8dae62bff7295b07c5
fatihduman060 <fatihduman060@gmail.com>
2026-06-30 11:03:53 +0300

    Cari döviz bakiyesi scriptini panele bağla

--- muhasebe/layout.php
+++ muhasebe/layout.php
@@ -174,6 +174,7 @@ function page_footer(): void
   <script src="assets/muhasebe-polish.js?v=1"></script>
   <script src="assets/teklif-hesap-fix.js?v=2"></script>
   <script src="assets/cek-vade-uyari.js?v=2"></script>
+  <script src="assets/cari-doviz-bakiye.js?v=1"></script>
 </body>
 </html>
 <?php }
