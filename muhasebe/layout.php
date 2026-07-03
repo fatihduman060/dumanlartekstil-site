13855da10301d3ea79a90d5dd1e5769d3b5a9a1e
fatihduman060 <fatihduman060@gmail.com>
2026-07-03 19:28:17 +0300

    Çek liste toplam scriptini panele bağla

--- muhasebe/layout.php
+++ muhasebe/layout.php
@@ -182,6 +182,7 @@ function page_footer(): void
   <script src="assets/dashboard-cari-pozisyon.js?v=3"></script>
   <script src="assets/dashboard-nakit-cek-detay.js?v=1"></script>
   <script src="assets/cari-hareket-kaynak.js?v=1"></script>
+  <script src="assets/cek-liste-toplam.js?v=1"></script>
 </body>
 </html>
 <?php }
