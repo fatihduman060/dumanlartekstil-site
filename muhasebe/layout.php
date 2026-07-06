b7b0b41554317ae06be950fd631db3e3c60be25d
fatihduman060 <fatihduman060@gmail.com>
2026-07-06 21:49:54 +0300

    Banka detay scriptini panele bağla

--- muhasebe/layout.php
+++ muhasebe/layout.php
@@ -185,6 +185,7 @@ function page_footer(): void
   <script src="assets/cari-hareket-kaynak.js?v=1"></script>
   <script src="assets/cek-liste-toplam.js?v=1"></script>
   <script src="assets/maas-excel-aktar.js?v=1"></script>
+  <script src="assets/hesap-banka-detay.js?v=1"></script>
 </body>
 </html>
 <?php }
