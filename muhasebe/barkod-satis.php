588b235549ee9bbbd3e6f041f641e5f4d19c1756
fatihduman060 <fatihduman060@gmail.com>
2026-08-27 11:39:44 +0300

    Barkodlu satışta hızlı fiyat değişimini aktif et

--- muhasebe/barkod-satis.php
+++ muhasebe/barkod-satis.php
@@ -86,6 +86,7 @@ page_header('Barkodlu Satış', 'barkod_satis');
   </section>
 </div>
 <script src="assets/zxing-browser-0.1.5.min.js?v=1"></script>
+<script src="assets/barkod-hizli-fiyat.js?v=1"></script>
 <script src="assets/barkod-kamera.js?v=2"></script>
 <script src="assets/barkod-satis.js?v=8"></script>
 <?php page_footer(); ?>
