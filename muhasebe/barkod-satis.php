dd40ec6eb1d95ea007908a08924ea0d8138efa26
fatihduman060 <fatihduman060@gmail.com>
2026-09-20 00:59:55 +0300

    POS: refresh abandoned-cart history asset

--- muhasebe/barkod-satis.php
+++ muhasebe/barkod-satis.php
@@ -163,7 +163,7 @@ page_header('Barkodlu Satış', 'barkod_satis');
 <script src="assets/barkod-canli-arama.js?v=8"></script>
 <script src="assets/barkod-veresiye-yeni-kisi.js?v=1"></script>
 <script src="assets/barkod-cuma-hizli-satis.js?v=2"></script>
-<script src="assets/barkod-satis-gecmis.js?v=14"></script>
+<script src="assets/barkod-satis-gecmis.js?v=15"></script>
 <script src="assets/barkod-veresiye-tahsilat.js?v=1"></script>
 <script src="assets/barkod-pos-hizli.js?v=4"></script>
 <?php page_footer(); ?>
