927d2810b348ec6d9bfa5405f3a3571f819d9431
fatihduman060 <fatihduman060@gmail.com>
2026-09-20 23:08:34 +0300

    Magaza: refresh card settlement display asset

--- muhasebe/magaza.php
+++ muhasebe/magaza.php
@@ -232,7 +232,7 @@ body.store-sales-user .magaza-satis-list td:before{content:attr(data-label);font
 .magaza-veresiye-card{order:4}
 }
 </style>
-<script src="assets/magaza-odeme-dagilimi.js?v=9"></script>
+<script src="assets/magaza-odeme-dagilimi.js?v=10"></script>
 <script src="assets/magaza-veresiye-manuel.js?v=3"></script>
 <script src="assets/magaza-nakit-toplam-fix.js?v=1"></script>
 <script src="assets/magaza-gunluk-satis.js?v=12"></script>
